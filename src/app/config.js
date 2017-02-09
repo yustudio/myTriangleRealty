f01eb019150991896fb084d186d3745f9178ee18
desongyu <desong.e.yu@gmail.com>
2017-02-09 12:43:08 -0500

    link to firebase

---
+++ src/app/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  // Change this to your firebase configuration! (Add Firebase to your web app)
+    FIREBASE_CONFIG: {
+
+    apiKey: "AIzaSyA6HNsTO789jIlpP0Bdi2ZpdLW69CiK6jg",
+    authDomain: "test-todo-c17e0.firebaseapp.com",
+    databaseURL: "https://test-todo-c17e0.firebaseio.com",
+    storageBucket: "test-todo-c17e0.appspot.com",
+    messagingSenderId: "1030744307265"
+
+        // apiKey: 'AIzaSyAAzWIkcMqyHueh1yQt84HdpMFfzL1OZYc',
+        // authDomain: 'react-redux-firebase-d6283.firebaseapp.com',
+        // databaseURL: 'https://react-redux-firebase-d6283.firebaseio.com',
+        // storageBucket: 'react-redux-firebase-d6283.appspot.com',
+        // messagingSenderId: '425723634684',
+
+    },
+};
