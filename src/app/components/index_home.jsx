1ebb7b874bbeef446856533a85228b8c3fa10dbc
desongyu <desong.e.yu@gmail.com>
2017-03-06 14:13:37 -0500

    add link to add expense

--- src/app/components/index_home.jsx
+++ src/app/components/index_home.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Link, IndexLink } from 'react-router';
 
 export default () => {
-  return <div> Home Page of our application! </div>;
+  return (
+  	<div> 
+
+  		<div>Home Page of our application! </div>
+
+  	<button className="submitButton" type="submit"> 
+		<IndexLink activeClassName="activeLink" to='/expenses'>Add Expense</IndexLink>
+	</button>
+
+	</div>
+  	);
 };
